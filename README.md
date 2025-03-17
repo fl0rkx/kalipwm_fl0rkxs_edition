0ef3b69ac2736bce8a95e5059f8335aad7ff0513
fl0rkx <203634119+fl0rkx@users.noreply.github.com>
2025-03-18 00:40:14 +0100

    Añadidos mis dotfiles

--- README.md
+++ README.md
@@ -1,6 +1,89 @@
+# KaliPWM
+
+Despliega un entorno de hacking profesional para Kali Linux ejecutando solo un script.
+
+![kalipwm-1](https://github.com/user-attachments/assets/689ad857-c6cd-464a-bbe2-227f4ae55299)
+![kalipwm-2](https://github.com/user-attachments/assets/8daca913-1975-4bcd-8f92-56fa418a7371)
+
+## Instalación y uso
+
+- Se recomienda el uso de una instalación nueva/limpia de Kali Linux.
+- Testado en Kali Linux 2024.3 con VMware, VirtualBox y Bare Metal.
+
+```
+git clone https://github.com/afsh4ck/kalipwm.git
+cd kalipwm
+bash kalipwm.sh
+sudo reboot
+```
+- Una vez reiniciado cambia a bspwm en la pantalla de inicio de sesión
+- El fondo de pantalla se toma de ~/Wallpapers/wallpaper.*
+- Video completo del entorno: https://youtu.be/3clLjO8W7Q4?si=GupOi6Bqwuu2O9Wk
+
+## Comandos
+
+Nota: En MacOS, cambia Windows por Command, y Alt por Option
+
+| Comando                     | Descripción                                                 |
+|-----------------------------|-------------------------------------------------------------|
+| Clic derecho en Polybar     | Cambia el tema de Polybar usando el menú del clic derecho   |
+| Windows + 1,2,3,4           | Navega entre escritorios                                    |
+| Windows + Enter             | Abre una nueva terminal                                     |
+| Windows + Enter             | Divide la terminal actual                                   |
+| Windows + Flechas           | Navega entre ventanas abiertas                              |
+| Windows + Tab               | Cambia entre los dos escritorios más recientes              |
+| Windows + W                 | Cierra la terminal actual                                   |
+| Windows + Alt + R           | Recarga el entorno de escritorio                            |
+| Windows + Alt + Q           | Reiniciar BSPWM                                             |
+| Windows + Alt + Flechas     | Redimensiona la ventana actual                              |
+| Windows + Shift + F         | Abre Firefox                                                |
+| Windows + Shift + B         | Abre Burp Suite                                             |
+| Windows + Shift + 1,2,3,4   | Mueve la ventana actual a otro escritorio                   |
+| Windows + Shift + Flechas   | Mueve la ventana actual                                     |
+| Ctrl + Shift + -+           | Cambia el tamaño del texto en la terminal                   |
+| Ctrl + T                    | Abre un navegador avanzado desde la terminal                |
+| .config/sxhkd/sxhkdrc       | Archivo de configuración de atajos (sxhkd)                  |
+| .config/bspwm/bspwmrc       | Archivo de configuración de BSPWM                           |
+| .config/polybar             | Carpeta con temas de Polybar                                |
+| .config/kitty/kitty.conf    | Archivo de configuración predeterminado para el terminal Kitty  |
+| ~/Wallpapers                | Carpeta de fondos de pantalla. Solo se permite un archivo llamado wallpaper.jpg  |
+| target 10.0.0.1             | Selecciona una IP de destino mostrada en Polybar            |
+| target reset                | Elimina el objetivo seleccionado                            |
+| tmux                        | Cambia la terminal a tmux                                   |
+| tmux —help                  | Muestra la ayuda de tmux                                    |
+| p10k configure              | Configura el tema de terminal Powerlevel10K                 |
+| .zshrc                      | Archivo de configuración de ZSH y alias de comandos         |
+| bpython                     | Python interactivo en la terminal                           |
+
+## Paquetes incluídos:
+
+```
+Bspwm
+Polybar
+Oh my zsh + Plugins
+Powerlevel10k
+Hack Nerd Fonts
+Python + pip + bpython
+Tmux + Oh my tmux
+Kitty
+lsd
+Neofetch
+Batcat
+Scrot
+feh
+Rofi
+Sxhkd
+Picom
+Neovim
+```
 
 ## Créditos
-- Autor de este proyecto:   fl0rkx
-- Autor del proyecto principal:       afsh4ck 
+- Autor:       afsh4ck 
 - Instagram:   <a href="https://www.instagram.com/afsh4ck">afsh4ck</a>
 - Youtube:     <a href="https://youtube.com/@afsh4ck">afsh4ck</a>
+
+## Soporte
+
+<a href="https://www.buymeacoffee.com/afsh4ck" rel="nofollow"><img width="250" align="left">
+![buy-me-a-coffe](https://github.com/user-attachments/assets/8c8f9e81-334e-469e-b25e-29888cfc9fcc)
+</a>
---
+++ kalipwm.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+# Comprobar si el usuario actual es root
+if [ "$UID" -eq 0 ]; then
+    echo "No se puede ejecutar como root."
+    exit 1
+else
+    # Comprobar si se está usando sudo
+    if [ -n "$SUDO_USER" ]; then
+        echo "No uses sudo"
+        exit 1
+    fi
+fi
+
+echo "
+██╗  ██╗ █████╗ ██╗     ██╗██████╗ ██╗    ██╗███╗   ███╗
+██║ ██╔╝██╔══██╗██║     ██║██╔══██╗██║    ██║████╗ ████║
+█████╔╝ ███████║██║     ██║██████╔╝██║ █╗ ██║██╔████╔██║
+██╔═██╗ ██╔══██║██║     ██║██╔═══╝ ██║███╗██║██║╚██╔╝██║
+██║  ██╗██║  ██║███████╗██║██║     ╚███╔███╔╝██║ ╚═╝ ██║
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝      ╚══╝╚══╝ ╚═╝     ╚═╝
+"
+sleep 2
+echo -e "[+] Script de automatización de entorno de hacking profesional."
+echo -e "[+] @afsh4ck - Sígueme en: YouTube, Instagram, TikTok"
+sleep 3
+echo -e "\n[*] Configurando la instalación..\n"
+sleep 4
+
+RPATH=`pwd`
+
+# Actualizar y actualizar todo
+sudo apt update && sudo apt -y full-upgrade
+
+# Instalar paquetes
+sudo apt install -y git vim feh scrot scrub zsh rofi xclip xsel locate neofetch wmname acpi bspwm sxhkd \
+imagemagick ranger tmux python3-pip font-manager lsd 
+
+# Instalar dependencias del entorno
+sudo apt install -y build-essential libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev \
+libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev # (xcb removed)
+
+# Instalar requisitos de polybar
+sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev \
+libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev \
+libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev \
+libmpdclient-dev libuv1-dev libnl-genl-3-dev
+
+# Instalar dependencias de picom
+sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
+libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev \
+libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev \
+uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
+
+# Instalar fuentes
+mkdir /tmp/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip -O /tmp/fonts/Hack.zip
+unzip /tmp/fonts/Hack.zip -d /tmp/fonts
+font-manager -i /tmp/fonts/*.ttf
+
+# Instalar ohmyzsh
+rm -rf ~/.oh-my-zsh
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm -f ~/.p10k.zsh
+cp -v $RPATH/CONFIGS/p10k.zsh ~/.p10k.zsh
+
+# Instalar plugins de zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+rm -f ~/.zshrc
+# ¿Instalar zsh-autocomplete?
+cp -v $RPATH/CONFIGS/zshrc ~/.zshrc
+
+# Instalar fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+
+# .tmux
+rm -rf ~/.tmux
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+ln -s -f ~/.tmux/.tmux.conf ~/
+cp -v $RPATH/CONFIGS/tmux.conf.local ~/.tmux.conf.local
+
+# nvim
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -O /tmp/nvim-linux64.tar.gz
+sudo tar xzvf /tmp/nvim-linux64.tar.gz --directory=/opt
+sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
+sudo rm -f /opt/nvim-linux64.tar.gz
+
+# nvchad - necesita trabajo. Bloquear cursor e interacción del usuario
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+
+# batcat
+wget https://github.com/sharkdp/bat/releases/download/v0.24.0/bat_0.24.0_amd64.deb -O /tmp/bat.deb
+sudo dpkg -i /tmp/bat.deb
+
+# Clonar repositorios de polybar & picom
+mkdir ~/github
+git clone --recursive https://github.com/polybar/polybar ~/github/polybar
+git clone https://github.com/ibhagwan/picom.git ~/github/picom
+
+# Instalar polybar
+cd ~/github/polybar
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
+
+# Instalar temas de polybar
+git clone --depth=1 https://github.com/adi1090x/polybar-themes.git ~/github/polybar-themes
+chmod +x ~/github/polybar-themes/setup.sh
+cd ~/github/polybar-themes
+echo 1 | ./setup.sh
+
+# Instalar picom
+cd ~/github/picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
+# Cambiar zona horaria
+# Para listar zonas horarias ejecutar: timedatectl list-timezones
+sudo timedatectl set-timezone "Europe/Bruselas"
+
+mkdir ~/screenshots
+# Copiar todos los archivos de configuración
+cp -rv $RPATH/CONFIGS/config/* ~/.config/
+
+# Copiar scripts
+cp -rv $RPATH/SCRIPTS/* ~/.config/polybar/forest/scripts/
+sudo ln -s ~/.config/polybar/forest/scripts/target.sh /usr/bin/target
+sudo ln -s ~/.config/polybar/forest/scripts/screenshot.sh /usr/bin/screenshot
+
+# Copiar wallpapers
+mkdir ~/Wallpapers/
+cp -rv $RPATH/WALLPAPERS/* ~/Wallpapers/
+
+# Establecer permisos de ejecución
+chmod +x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/bspwm/scripts/bspwm_resize
+chmod +x ~/.config/polybar/launch.sh
+chmod +x ~/.config/polybar/forest/scripts/target.sh
+chmod +x ~/.config/polybar/forest/scripts/screenshot.sh
+
+# Seleccionar tema de rofi
+# rofi-theme-selector
+
+# Habilitar toque para hacer clic y cambiar dirección de desplazamiento del touchpad (portátiles) https://cravencode.com/post/essentials/enable-tap-to-click-in-i3wm/
+# sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+# Section "InputClass"
+#        Identifier "touchpad"
+#        MatchIsTouchpad "on"
+#        Driver "libinput"
+#        Option "Tapping" "on"
+#        Option "NaturalScrolling" "on"
+# EndSection
+#
+# EOF
+
+# Limpiar archivos
+rm -rf ~/github
+rm -rf $RPATH
+sudo apt autoremove -y
+
+echo -e "\n[+] Entorno desplegado, Happy Hacking ;) \n"
+echo -e "\n[+] Por favor, reinicia el equipo (sudo reboot) \n"
